8d941e528f094b435688ece3fab691891dbaca89
Tangkoan <kuytangkoan@gmail.com>
2025-09-01 09:00:10 +0700

    55.Intro & Documentation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
+import {CssBaseline} from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./Movies/Movies";
+import Actors from "./Actors/Actors";
+import NotFound from "./NotFound/NotFound";
+
 const App = () => {
     return (
-        <>
-        HomePage
-        </>
-    )
+      <div>
+        <CssBaseline />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Movies/>}/>
+                    <Route path="/actors/:id" element={<Actors/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,11 @@
+import { Movie } from "@mui/icons-material"
+
+const Movies = () => {
+return(
+    <div>
+        <h1>Movies Page</h1>
+    </div>
+)
+}
+
+export default Movies;
