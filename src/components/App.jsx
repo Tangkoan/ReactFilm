049ce74428e32cdd21ba1876c887166d6fec3a9e
Tangkoan <kuytangkoan@gmail.com>
2025-09-01 09:12:31 +0700

    56. Component Structure & Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Movies from "./Movies/Movies";
 import Actors from "./Actors/Actors";
 import NotFound from "./NotFound/NotFound";
+import NavBar from "./NavBar/NavBar";
+import { Movie } from "@mui/icons-material";
+import Profile from "./Profile/Profile";
 
 const App = () => {
     return (
       <div>
         <CssBaseline />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Movies/>}/>
-                    <Route path="/actors/:id" element={<Actors/>}/>
-                    <Route path="*" element={<NotFound/>}/>
-                </Routes>
-            </BrowserRouter>
+        <BrowserRouter>
+          <NavBar />
+          <main>
+            <div>
+              <Routes>
+                <Route path="/movie/:id" element={<Movie />} />
+                <Route path="/" element={<Movies />} />
+                <Route path="/actors/:id" element={<Actors />} />
+                <Route path="*" element={<NotFound />} />
+                <Route path="/profile/:id" element={<Profile/>}/>
+              </Routes>
+            </div>
+          </main>
+        </BrowserRouter>
       </div>
     );
 }
